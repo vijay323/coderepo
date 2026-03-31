5290875477005c7e25f159fecac9ef0df060e6c6
Vijay <vjykmr2003@gmail.com>
2026-03-31 23:15:48 +0530

    update

---
+++ Python/inheritance.py
@@ -0,0 +1,35 @@
+class Payment:
+
+    def __init__(self, amount):
+        self.amount = amount
+
+    def pay(self):
+        print("Processing payment...")
+
+
+class UPI(Payment):
+
+    def pay(self):
+        print(f"Paid rs{self.amount} using UPI")
+
+
+class Card(Payment):
+
+    def pay(self):
+        print(f"Paid rs{self.amount} using Card")
+
+
+class NetBanking(Payment):
+
+    def pay(self):
+        print(f"Paid rs{self.amount} using Net Banking")
+
+
+# usage
+p1 = UPI(1000)
+p2 = Card(2000)
+p3 = NetBanking(3000)
+
+p1.pay()
+p2.pay()
+p3.pay()
