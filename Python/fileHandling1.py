32343cf3c7f90b82dbc02fa067695d83925b462a
Vijay <vjykmr2003@gmail.com>
2026-04-04 15:19:42 +0530

    file handling

--- Python/fileHandling1.py
+++ Python/fileHandling1.py
@@ -1,3 +1,9 @@
-f=open('sample.txt','w')
-f.write('hello world')
-f.close
+# f=open('sample.txt','w')
+# f.write('hello world')
+# f.close
+
+# f=open('Hello.txt','w')
+# f.write('Hello \nNEw line')
+# f.write('\nHow are you')
+# f.close()
+
