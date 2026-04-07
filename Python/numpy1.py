de801ae5ce9f8fe53bc0620c9961a08853ba3220
Vijay <vjykmr2003@gmail.com>
2026-04-07 16:44:07 +0530

    programs

---
+++ Python/numpy1.py
@@ -0,0 +1,75 @@
+import numpy as np
+
+# a=np.array([1,2,3])
+# print(a)
+
+# b=np.array([['a','b','c'],[6,7,8]])
+# print(b)
+
+# c=np.array([[[1,2,3],[4,2,40],[13,42,1]]])
+# print(c)
+
+# print(np.array([1,3,2,142,21],dtype=float))
+
+# print(np.arange(1,21,2))
+# print(np.arange(100,20,-5).reshape(2,8))
+
+# print(np.ones(6))
+# print(np.zeros((6,6)))
+
+# print(np.random.random(5))
+
+# print(np.linspace(1,10,10))
+
+# print(np.identity(6))
+
+# a1 = np.arange(10,dtype=int)
+# a2=np.arange(10,dtype=float)
+# a3=np.arange(27).reshape(3,3,3)
+
+# print(a3.ndim)
+
+# print(a2.shape)
+
+# print(a3.size)
+
+# print(a1.itemsize)
+
+# a3=a3.astype(np.int32)
+# print(a3.dtype)
+
+# print(a1**2)
+
+# print(a1==8)
+
+# print(a1*a2)
+
+# a1=np.random.random((3,3))
+# a1=np.round(a1*100)
+# print(a1)
+# print(np.max(a1))
+# print(np.min(a1,axis=1))
+# print(np.sum(a1,axis=1))
+# print(np.sum(a1))
+# print(np.prod(a1))
+a2=np.array([[0,1,2,3],
+             [4,5,6,7],
+             [8,9,10,11]])
+ 
+# print(a2[::2,1::2])
+# print(a2[1,::3])
+
+# print(a2[:2,1:])
+
+# a3=np.array([[[0,1],
+#               [2,3]],
+              
+#               [[4,5],
+#                [6,7]]])
+
+# a4=np.arange(27).reshape(3,3,3)
+# print(a4[0,1])
+# print(a4[1,:,1])
+# print(a4[2,1:,1:])
+
+# print(a4[0::2,0,::2])
